bf084d67defc72835809d4480384cf4945f06667
bobby-brice <bobby.brice@gmail.com>
2020-06-03 18:40:23 +0000

    created a callback to setupInput and a function to handleUserInput

--- play.js
+++ play.js
@@ -2,3 +2,26 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(data) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+};
+
+setupInput();
+
+const handleUserInput = function(key) {
+  console.log(key);
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+

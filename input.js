dc95902f32730739cbd905dbcfb1619db6dd04db
bobby-brice <bobby.brice@gmail.com>
2020-06-03 19:58:23 +0000

    added some fun messages on keystrokes

--- input.js
+++ input.js
@@ -2,21 +2,42 @@
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function(data) {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => handleUserInput(key));
+  stdin.on('data', (key) => handleUserInput(key, connection));
   return stdin;
 };
 
 setupInput();
 
-const handleUserInput = function(key) {
+const handleUserInput = function(key, conn) {
   console.log(key);
   if (key === "\u0003") {
     process.exit();
+  }
+  if (key === "\u0077") {
+    conn.write("Move: up");
+  }
+  if (key === "\u0073") {
+    conn.write("Move: down");
+  }
+  if (key === "\u0061") {
+    conn.write("Move: left");
+  }
+  if (key === "\u0064") {
+    conn.write("Move: right");
+  }
+  if (key === "\u0020") {
+    conn.write("Say: AHHHHH");
+  }
+  if (key === "\u0066") {
+    conn.write("Say: HANGGRYYY");
   }
 };
 
